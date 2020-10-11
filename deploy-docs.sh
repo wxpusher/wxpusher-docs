3c2a15c7915a9d1e09d4167be6d4aee381cc02c6
zhangjie33 <zhangjie33@meituan.com>
2020-10-11 20:07:26 +0800

    忽略部署脚本

---
+++ deploy-docs.sh
@@ -0,0 +1,43 @@
+
+if [[ $1 == "-p" ]]; then
+  deployPath=/data/wxpusher/wxpusher-docs/docs/
+  server=139.198.188.161
+  port=20122
+  echo "部署生成环境"
+else
+  deployPath=/data/app/wxpusher-test/wxpusher-docs/docs/
+  server=115.28.141.240
+  port=22
+  echo "部署测试环境"
+fi
+
+
+targetFile=dist.tar.gz
+
+echo "打包产物"
+
+tar -zcf  ${targetFile} -C docs .    
+
+echo "上传"
+
+ssh -p ${port} admin@${server} "
+if [ ! -d ${deployPath} ]; then
+    echo  "文件夹不存在，创建文件夹"
+    mkdir -p ${deployPath}
+fi
+"
+echo "上传文件:${targetFile}"
+scp -P ${port} ${targetFile} admin@${server}:${deployPath}${targetFile}
+echo "上传完成"
+
+echo "解压"
+ssh -p ${port} admin@${server} "
+tar -zxf ${deployPath}${targetFile} -C ${deployPath}
+rm ${deployPath}${targetFile}
+"
+rm ${targetFile}
+if [[ $? == 0 ]]; then
+    echo "\033[32m 部署完成"
+else
+    echo "\033[31m 部署失败"
+fi
